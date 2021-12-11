99a4440d98d67bc2490bddf501f6b31c64963167
rob <bierman.0.rob@gmail.com>
2021-12-10 23:01:10 -0800

    pass cells to scoring, not hdf5

--- src/SRRS/cell_level_processing.py
+++ src/SRRS/cell_level_processing.py
@@ -6,23 +6,19 @@ from . import metrics
 
 import sys
 
-def score_cells(hdf5, metric, min_gene_spots=10):
+def score_cells(cells, metric):
 
     available_metrics = {
         '_test':metrics._test,
         'peripheral':metrics.peripheral,
     }
 
-
     if metric not in available_metrics:
         sys.stderr.write('Metric {} not found in possible metrics {}\n'.format(metric, available_metrics.keys()))
         sys.exit(1)
 
-    #h5py objects cannot be passed into multiprocessing since they are not pickleable
-    #workaround is for each process to be pased the hdf5 path and the cell name to operate on
-    #each process then needs to open the hdf5 file, which is something I'd like to avoid as a future TODO
     with multiprocessing.Pool() as p:
-        results = p.map(available_metrics[metric], hdf5.iter_cells())
+        results = p.map(available_metrics[metric], cells)
 
     return results
 
--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -5,7 +5,7 @@ import SRRS
 @pytest.mark.parametrize('metric', ['_test','peripheral'])
 def test_scoring_metrics_m1s4(dataset, metric, request):
     dataset = request.getfixturevalue(dataset)
-    score_dfs = SRRS.score_cells(dataset, metric=metric)
+    score_dfs = SRRS.score_cells(dataset.iter_cells(), metric=metric)
     assert len(score_dfs) == dataset.num_cells
 
     cell_ids = [c for df in score_dfs for c in df.cell_id.unique()]
