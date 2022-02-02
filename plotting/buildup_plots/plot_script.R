6ebeae168cb62d33c62b118e7ec67efc3cf95f89
Rob <rbierman@stanford.edu>
2022-02-02 15:19:45 -0800

    Improvements to ReadZs buildup plots

--- plotting/buildup_plots/plot_script.R
+++ plotting/buildup_plots/plot_script.R
@@ -1,94 +1,42 @@
+#Automating gene plotting
+#Need a table of gene/ontology with mean RZS score
+
 library(Gviz)
 library(GenomicFeatures)
 library(data.table)
 
-#support for plus/minus strand bam plotting from:
-#https://support.bioconductor.org/p/56047/
-library(Rsamtools)
-strandedBamImport <- function (file, selection) {
-    if (!file.exists(paste(file, "bai", sep = ".")))
-        stop("Unable to find index for BAM file '", file, "'. You can
-build an index using the following command:\n\t",
-             "library(Rsamtools)\n\tindexBam(\"", file, "\")")
-    sinfo <- scanBamHeader(file)[[1]]
-    res <- if (!as.character(seqnames(selection)[1]) %in%
-               names(sinfo$targets)) {
-        mcols(selection) <- DataFrame(score = 0)
-        selection
-    }else {
-        param <- ScanBamParam(what = c("pos", "qwidth", "strand"),
-                              which = selection, flag =
-scanBamFlag(isUnmappedQuery = FALSE))
-        x <- scanBam(file, param = param)[[1]]
-        gr <- GRanges(strand=x[["strand"]], ranges=IRanges(x[["pos"]],
-width = x[["qwidth"]]), seqnames=seqnames(selection)[1])
-        grs <- split(gr, strand(gr))
-        cov <- lapply(grs[c("+", "-")], function(y)
-coverage(ranges(y),
-width=end(selection)))
-        pos <- sort(unique(unlist(lapply(cov, function(y) c(start(y),
-end(y))))))
-        if(length(pos)==0){
-            mcols(selection) <- DataFrame(plus=0, minus=0)
-            selection
-        }else{
-            GRanges(seqnames = seqnames(selection)[1],
-ranges=IRanges(start=head(pos, -1), end=tail(pos, -1)),
-                    plus=as.numeric(cov[["+"]][head(pos, -1)]),
-                    minus=-as.numeric(cov[["-"]][head(pos, -1)]))
-        }
-    }
-    return(res)
-}
-                                         
+options(ucscChromosomeNames=FALSE)
 
 args = commandArgs(trailingOnly=TRUE)
-
 gene_name = args[1]
+gene_padding = 0.05 #how much to pad upstream and downstream of the gene
 
 txdb_path = "txdb.mm10.sqlite"
 bed_path = "MERFISH_genes.bed"
-srrs_path = "/oak/stanford/groups/horence/rob/isoform_localizations/SRRS/outputs/gene_ontology/MOp_periphal_ReadZs_10X_gene_ontology.csv"
 bam_stem = "/scratch/groups/horence/rob/data/MERFISH_scRNAseq/10X_mapping/merged_by_celltype/"
-                                         
+rzs_score_path = "/oak/stanford/groups/horence/rob/isoform_localizations/SRRS/analysis/gene_ontology_rzs.csv"
 bam_paths = Sys.glob(paste(bam_stem,"*.bam",sep=""))
-bam_ontologies = lapply(lapply(bam_paths,basename),tools::file_path_sans_ext)
-                                         
-#find which cell-types to plot from SRRS
-srrs_df = fread(srrs_path)
-srrs_df = srrs_df[gene == gene_name] #filter to results from the gene of interest
-srrs_df = srrs_df[bh_p < 0.05] #filter by bh_p
-srrs_df = srrs_df[order(med_score)] #order by effect size
-srrs_df = srrs_df[ontology %in% bam_ontologies] #keep only ontologies which have corresponding bams
-srrs_df = unique(srrs_df,by='ontology') #deduplicate by ontology
-
-n_onts_to_plot = 6
-                                         
-if(nrow(srrs_df) == 0){
-    print("No matching cell-types")
-    stop()
-} else if (nrow(srrs_df) <= n_onts_to_plot){
-    #plot all cell types
-    n_onts_to_plot = nrow(srrs_df)
-    cell_types = srrs_df$ontology
-    effect_sizes = srrs_df$med_score
-} else {
-    #plot top n/2 and bottom n/2 cell-types by med score (effect size)
-    n = n_onts_to_plot/2
-    cell_types = c(head(srrs_df,n)$ontology, tail(srrs_df,n)$ontology)
-    effect_sizes = c(head(srrs_df,n)$med_score, tail(srrs_df,n)$med_score)
-}
-
                                          
 gene_locs = fread(bed_path)
 gene_row = gene_locs[gene == gene_name]
 
+gene_buffer = (gene_row$end-gene_row$start)*gene_padding
+
 chr = gene_row[, get("#chr")]
-window_start = gene_row$start
-window_end   = gene_row$end
+window_start = round(gene_row$start-gene_buffer)
+window_end   = round(gene_row$end+gene_buffer)
+gene_strand  = gene_row$strand
 outfile = paste(gene_name,".png",sep="")
 tracks = list()
-                                         
+
+df = fread(rzs_score_path)
+df = df[gene == gene_name]
+df = df[order(mean_rzs)]
+
+cell_types = df$ontology
+
+stopifnot(length(cell_types) > 0)
+ 
 ####################################
 #                                  #
 #                                  #
@@ -103,40 +51,41 @@ geneTrack = GeneRegionTrack(
     chromosome = chr,
     start = window_start,
     end = window_end,
+    strand = gene_strand,
     name = gene_name,
 )
 
 geneTrack <- geneTrack[
-    feature(geneTrack) %in% c("CDS", "UTR")
+    gene(geneTrack) == gene_name
 ]
 tracks = append(tracks,geneTrack)
 
-gTrack <- GenomeAxisTrack()                                         
-tracks = append(tracks,gTrack)
+####################################
+#                                  #
+#                                  #
+#            Data Tracks           #
+#                                  #
+#                                  #
+####################################
+data_tracks = list()
 
-####################################
-#                                  #
-#                                  #
-#            Data Track            #
-#                                  #
-#                                  #
-####################################
-for(i in 1:n_onts_to_plot){
-    ct = cell_types[[i]]
-    ct = sub(" ","_",ct)
-    
-    effect_size = effect_sizes[[i]]
-    effect_size = format(round(effect_size, 2), nsmall = 2)
-    
-    cov_track = DataTrack(
+for(ct in cell_types){
+        
+    cov_track = AlignmentsTrack(
         range = paste(bam_stem,ct,".bam",sep=""),
-        name = paste(ct,effect_size,sep="\n"),
+        name = ct,
         chromosome = chr,
-        importFunction=strandedBamImport,
-        stream=TRUE,
+        #col = colors[[i]],
+        #fill = colors[[i]],
+        strand = gene_strand,
+        #legend = TRUE,
+        type = "coverage",
+        window = -1,
+        windowSize = 1,
     )
-    tracks = append(tracks,cov_track)
+    data_tracks = append(data_tracks,cov_track)
 }
+tracks = append(tracks, data_tracks)
 
 ####################################
 #                                  #
@@ -145,21 +94,18 @@ for(i in 1:n_onts_to_plot){
 #                                  #
 #                                  #
 ####################################
-png(outfile, width = 800, height = 800)
+png(outfile, width = 800, height = 400)
 par(mar=c(5,6,4,1)+.1)
+
 plotTracks(
     tracks,
-    type = "h",
+    sizes = rep(1/length(tracks),length(tracks)), #give equal vertical space to each plot
     from = window_start, 
     to = window_end,
-    thinBoxFeature = "UTR",
+    thinBoxFeature = c("utr3","utr5"),
     collapse = F,
     transcriptAnnotation = "gene",
-    col=c("red","blue"),
-    groups=c("+", "-"),
-    fontcolor.exon = "black",
-    legend = F,
-    cex = 0.6
+    fontcolor.exon = "black"
 )
 
 dev.off()
